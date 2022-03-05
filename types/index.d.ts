fe4e2c504a7448d759a2e81e77a30e9adb743a96
Moussa Mokhtari <m_mokhtari@esi.dz>
2022-03-05 06:34:16 +0300

    Initial commit

---
+++ types/index.d.ts
@@ -0,0 +1,19 @@
+import {Plugin,App} from 'vue';
+
+import {NodeContext,PropsType} from './revsetter-maps';
+
+
+export default class BlocksTree {
+    static name: string;
+  
+    static install: (app: App,settings?:{nodeName:string,treeName:string}) => any;
+    $props: Record<string, any>;
+}
+
+export class BlocksNode {
+
+    static name: string;
+    $props: Record<string, any>;
+}
+
+export {NodeContext,PropsType}
